2099f6d31524f518d4564d9fcfc43346fa0634e4
aenelle <aenelle@gmail.com>
2018-04-03 14:08:50 +0300

    hotfixes

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,7 +3,7 @@
 
     dropdowns.forEach(dropdown => {
         let valElem = dropdown.querySelector('.dropdown-value');
-        let items = dropdown.querySelectorAll('.dropdown-list-item');
+        let items = dropdown.querySelectorAll('.js-dropdown-item');
 
         valElem.addEventListener('click', function() {
             if (dropdown.classList.contains('opened')) {
@@ -12,6 +12,16 @@
                 dropdown.classList.add('opened');
             }
         }, false);
+
+        items.forEach(r => {
+            r.addEventListener('click', function() {
+                items.forEach(r=> r.classList.remove('selected'));
+                this.classList.add('selected');
+                valElem.classList.add('dirty');
+                valElem.innerHTML = this.innerHTML;
+                dropdown.classList.remove('opened');
+            });
+        })
     });
 
     window.addEventListener('click', function(e) {
